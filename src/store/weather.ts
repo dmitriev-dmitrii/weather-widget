643073e625745e5acd2a7eb4fa50ddfdcad255e5
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-02-11 16:29:46 +0300

    add api

---
+++ src/store/weather.ts
@@ -0,0 +1,26 @@
+import {weatherApi} from "@/api";
+
+import _get from 'lodash/get';
+export default {
+    namespaced: true,
+    state: {
+        cityList:[]
+    },
+    getters: {
+        sortedCityList:(state:any)=>{
+            return state.cityList
+        }
+    },
+    mutations: {
+        addCityToList:(state : any ,city: any)=>{
+            state.cityList.push(city)
+        }
+    },
+    actions: {
+        getWeatherByCityName:async function (context:any,payload:any)  {
+
+            return  await weatherApi.getByCityName(payload);
+
+        }
+    },
+};
